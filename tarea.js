0710e916b4c3246d4343f16b064eaadfa25f96e9
Martin Fernandez <fernandezmartin.k26@gmail.com>
2023-05-09 13:25:02 -0300

    Agregando archivo tarea.js

---
+++ tarea.js
@@ -0,0 +1,18 @@
+// 1) Realizar una funcion que reciba un numero y escriba una piramide desde 1 hasta ese numero de la siguiente forma:
+// para valor 6
+// 1
+// 12
+// 123
+// 1234
+// 12356
+
+const piramide = numero => {
+    for(let i = 1; i <= numero; i++){
+        let fila = "";
+        
+        for(let j = 1; j<= i; j++){
+            fila += j;
+        }
+        console.log(fila);
+    }
+}
